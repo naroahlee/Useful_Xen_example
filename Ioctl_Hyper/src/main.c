64dc7c74211435a3a26df25e30fc80db02a6f848
naroahlee <naroahlee@gmail.com>
2017-03-09 23:16:20 -0600

    First Commit

---
+++ Ioctl_Hyper/src/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include "privcmd.h"
+
+#define XENVER_version 0
+#define __HYPERVISOR_xen_version (17)
+
+int main(int argc, char* argv[])
+{
+	int fd;
+	long s64xenver;
+
+    privcmd_hypercall_t call = {
+        .op = __HYPERVISOR_xen_version,
+        .arg = { XENVER_version, 0},
+    };
+
+	fd = open("/dev/xen/privcmd", O_RDWR|O_CLOEXEC);
+
+	if(-1 == fd)
+	{
+		fprintf(stderr, "Open Error!\n");
+	}
+
+	s64xenver = ioctl(fd, IOCTL_PRIVCMD_HYPERCALL, &call);
+
+	printf("Xen version %ld.%ld\n", s64xenver >> 16, s64xenver & 0xffff);
+	close(fd);
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Useful_Xen_example
 Very small hypercall examples
+
+xc_Hypercall: 
+An example of how to leverage libxenctrl to invoke a hypercall.
+libxenctrl is a hypercall wrapper.
+Only availiable in Dom0... Maybe available in DomU, but libxenctrl is needed.
+
+Ioctl_Hypercall:
+An example of how to use IOCTL to pass hypercall to a kernel module (driver)
+/dev/xen/privatecmd
+Avaliable in both Dom0 and DomU.
+The interface is somewhat more complicated.
+
+TBD:
+I haven't figure out how to invoke a Hypercall_domctl()
+
